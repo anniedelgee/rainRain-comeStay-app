4ee6f2ae578b6876f33124a000bdd040d18fbefb
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2023-03-24 13:37:45 +0000

    further changes made to how im going to cal the api

--- challenge/starter/assets/index.js
+++ challenge/starter/assets/index.js
@@ -1,31 +1,49 @@
-// Get the current date and time
-var now = moment();
+//api key
+let apiKey="22f003c1dfb906c0eac4952b94e79b73";
 
-// Format the date and time in a specific way
-var formattedDate = now.format('MMM D, YYYY');
-var formattedTime = now.format('h:mm A');
+//variables
+var city;
+var latitude;
+var longitude;
 
-// Define the API endpoint URL and parameters
-var apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-var apiKey = 'YOUR_API_KEY';
-var city = 'New York';
-var countryCode = 'US';
+var today = $("#today");
+var forecast = $("#forecast");
+var input = $(".form-input");
+var cityNow= $("#cityNow");
+var tempNow= $("#tempNow");
+var windNow= $("#windNow");
+var humidNow= $("#humidNow");
+var buttonlist = $(".buttonlist");
 
-// Build the full API URL with parameters
-var fullUrl = apiUrl + '?q=' + city + ',' + countryCode + '&appid=' + apiKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-// Use the Fetch API to retrieve the weather data
-fetch(fullUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Use the weather data in your dashboard
-    var temperature = data.main.temp;
-    var weatherDescription = data.weather[0].description;
-  });
-// Update the date and time elements in your HTML
-document.getElementById('date').textContent = formattedDate;
-document.getElementById('time').textContent = formattedTime;
-
-// Update the weather elements in your HTML
-document.getElementById('temperature').textContent = temperature;
-document.getElementById('weather-description').textContent = weatherDescription;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+  
+      // Log the queryURL
+      console.log(queryURL);
+  
+      // Log the resulting object
+      console.log(response);
+  
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      
+      // Convert the temp to Celsius
+      var tempC = response.main.temp - 273.15;
+  
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempC").text("Temperature (C) " + tempC.toFixed(2));
+  
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (C): " + tempC);
+    });
